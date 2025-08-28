e702b59bea14feb71d2d55fca44654ac1cdf2f5a
ivanmonahov75 <ivanmonahov75@gmail.com>
2025-08-28 16:01:08 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,197 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+// defines 
+#define BUTTON_UP 142 // change back to 14
+#define BUTTON_DOWN 15
+#define BUTTON_ON 16
+#define EMER_STOP 17
+
+#define BUTTON_TIMER 14 // change to another
+
+// functions 
+bool chech_button(const uint8_t BUTTON_NUM, bool* flag);
+
+// objects
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+void setup() {
+  // set communication and lcd
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("PlFm1.0");
+  lcd.setCursor(8, 0);
+  lcd.print("Timer:");
+  lcd.setCursor(0, 1);
+  lcd.print("Mode: ");
+  lcd.setCursor(10, 1);
+  lcd.print("Hz: ");
+
+  // set pins        
+
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
+  pinMode(BUTTON_ON, INPUT_PULLUP);
+  pinMode(BUTTON_ON, INPUT_PULLUP);
+  pinMode(EMER_STOP, INPUT_PULLUP);
+  pinMode(BUTTON_TIMER, INPUT_PULLUP);
+
+  
+
+}
+uint8_t speed = 15;
+uint8_t timer_mins = 5;
+bool on = 0;
+bool flag_on = 0;
+
+bool flag_up = 0;
+bool flag_down = 0;
+bool flag_timer = 0;
+
+void loop() {
+  if (digitalRead(17) == 1) // safety measure time
+  {
+    // do our shit
+    // note: all buttons are reversed
+
+// on button
+    if (!digitalRead(BUTTON_ON) && flag_on == 0)
+    {
+      delay(15);
+      if (!digitalRead(BUTTON_ON))
+      {
+        flag_on = 1;
+
+        // actual action
+        on = !on;
+      }
+    }
+    if (digitalRead(BUTTON_ON) && flag_on)
+    flag_on = !digitalRead(BUTTON_ON);
+
+
+// up button 
+    if (!digitalRead(BUTTON_UP) && flag_up == 0)
+    {
+      delay(15);
+      if (!digitalRead(BUTTON_UP))
+      {
+        flag_up = 1;
+
+        // actual action
+        speed++;
+      }
+    }
+    flag_up = !digitalRead(BUTTON_UP);
+
+
+// down button
+    if (!digitalRead(BUTTON_DOWN) && flag_down == 0)
+    {
+      delay(15);
+      if (!digitalRead(BUTTON_DOWN))
+      {
+        flag_down = 1;
+
+        // actual action
+        speed--;
+      }
+    }
+    flag_down = !digitalRead(BUTTON_DOWN);
+
+// timer button
+    if (!digitalRead(BUTTON_TIMER) && flag_timer == 0)
+    {
+      delay(15);
+      if (!digitalRead(BUTTON_TIMER))
+      {
+        flag_timer = 1;
+
+        // actual action
+        timer_mins++;
+        if (timer_mins > 10)
+        {
+          timer_mins = 1;
+        }
+      }
+    }
+    flag_timer = !digitalRead(BUTTON_TIMER);
+
+
+
+
+
+  }
+  else
+  {
+
+    // turn off
+
+
+
+  }
+
+
+  // set state lcd
+  lcd.setCursor(5, 1);
+  if (on)
+  {
+    lcd.print("ON ");
+  }
+  else
+  {
+    lcd.print("OFF");
+  }
+
+  // set speed to lcd
+  if (speed > 30)
+  {
+    speed = 30;
+  }
+  if (speed < 1)
+  {
+    speed = 1;
+  }
+
+  lcd.setCursor(14, 1);
+  if (speed > 9)
+  {
+    lcd.print(speed);
+  }
+  else
+  {
+    lcd.print(" ");
+    lcd.print(speed);
+  }
+
+  // set timer
+  lcd.setCursor(14, 0);
+
+  if (timer_mins != 10)
+  {
+    lcd.print(" ");
+  }
+  lcd.print(timer_mins);
+
+  // global delay
+  delay(10);
+}
+
+// bool chech_button(const uint8_t BUTTON_NUM, bool* flag){  
+//   bool res = false;
+//   if (!digitalRead(BUTTON_NUM) && flag == 0)
+//   {
+//     delay(15);
+//     if (!digitalRead(BUTTON_NUM))
+//     {
+//       *flag = 1;
+
+//       // actual action
+//       res = true;
+//     }
+//   }
+//   *flag = !digitalRead(BUTTON_NUM);
+//   return res;
+// }
+
